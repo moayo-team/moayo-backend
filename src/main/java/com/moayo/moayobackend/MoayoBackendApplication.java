e5e3580033581a27b65a8aedb155ac6b8e15d73e
Bae Suhyun <68588469+bsh-ko@users.noreply.github.com>
2026-01-20 00:52:31 +0900

    [chore] 프로젝트 세팅  (#9)
    * 🔧 chore: Feature, Bug 이슈 템플릿 생성 (#3)
    
    * ✨ feat: feature 이슈 템플릿 생성 (#4)
    
    * ✨ chore: Refactor 이슈 템플릿 생성 (#5)
    
    * Update feature  issue templates (#6)
    
    * ✨ chore: Deploy 이슈 템플릿 생성 (#7)
    
    * 🔧 chore: PR 템플릿 추가 (#8)
    
    * 🎉  프로젝트 세팅
    
    * 🎉  chore: 프로젝트 세팅
    
    * ✅ ci : 깃허브 ci workflow 추가
    
    * [TEST] ci test (#10)
    
    * ci test
    
    * ci test
    
    * Feature/ci test (#11)
    
    * ci test
    
    * ci test
    
    * ci test
    
    * ci test

---
+++ src/main/java/com/moayo/moayobackend/MoayoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moayo.moayobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoayoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoayoBackendApplication.class, args);
+    }
+
+}
