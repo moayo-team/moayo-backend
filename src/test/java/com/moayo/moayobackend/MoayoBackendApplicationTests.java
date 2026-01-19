0832d128b914807a126265f3efcf5b3219eb980b
bsh-ko <ssuh3872@gmail.com>
2026-01-19 20:51:38 +0900

    🎉  프로젝트 세팅

---
+++ src/test/java/com/moayo/moayobackend/MoayoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moayo.moayobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoayoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
