ca283da2adc2ced758c800d973ff67be9d9191b5
KIMSEOYEON <128014629+cse2348@users.noreply.github.com>
2026-01-24 16:52:28 +0900

    feat: 게시판 구현

---
+++ src/main/java/com/moayo/moayobackend/controller/PostController.java
@@ -0,0 +1,74 @@
+package com.moayo.moayobackend.controller;
+
+import com.moayo.moayobackend.entity.Category;
+import com.moayo.moayobackend.entity.Post;
+import com.moayo.moayobackend.dto.PostResponseDto;
+import com.moayo.moayobackend.service.PostService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/posts")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    // 모집글 리스트 조회
+    @GetMapping
+    public ResponseEntity<Page<PostResponseDto>> getAllPosts(
+            @RequestParam(required = false) Category category,
+            @PageableDefault(size = 10) Pageable pageable) {
+        return ResponseEntity.ok(postService.getPosts(category, pageable));
+    }
+
+    // 모집글 상세 정보 조회
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostResponseDto> getPostDetail(@PathVariable Long postId) {
+        return ResponseEntity.ok(postService.getPostDetail(postId));
+    }
+
+    // 새 모집글 등록
+    @PostMapping
+    public ResponseEntity<Long> create(@Valid @RequestBody Post post) {
+        return ResponseEntity.ok(postService.createPost(post));
+    }
+
+    // 내 게시글 수정
+    @PatchMapping("/{postId}")
+    public ResponseEntity<Void> update(
+            @PathVariable Long postId,
+            @RequestParam String title,
+            @RequestParam String content,
+            @RequestParam Integer count,
+            @RequestParam Category category,
+            @RequestParam String role,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate deadline) {
+
+        postService.updatePost(postId, title, content, category, role, count, deadline);
+        return ResponseEntity.ok().build();
+    }
+
+    // 내 게시글 삭제
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<Void> delete(@PathVariable Long postId) {
+        postService.deletePost(postId);
+        return ResponseEntity.noContent().build();
+    }
+
+    // 내 게시글 모아보기
+    @GetMapping("/me")
+    public ResponseEntity<Page<PostResponseDto>> getMyPosts(
+            @RequestParam String nickname,
+            @PageableDefault(size = 10) Pageable pageable) {
+        return ResponseEntity.ok(postService.getMyPosts(nickname, pageable));
+    }
+}
---
+++ src/main/java/com/moayo/moayobackend/service/PostService.java
@@ -0,0 +1,66 @@
+package com.moayo.moayobackend.service;
+
+import com.moayo.moayobackend.entity.Category;
+import com.moayo.moayobackend.entity.Post;
+import com.moayo.moayobackend.dto.PostResponseDto;
+import com.moayo.moayobackend.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+    // 모집글 리스트 조회 (카테고리별 필터링 가능)
+    @Transactional(readOnly = true)
+    public Page<PostResponseDto> getPosts(Category category, Pageable pageable) {
+        LocalDate today = LocalDate.now();
+        Page<Post> posts = (category == null) ?
+                postRepository.findAllByDeadlineGreaterThanEqualOrDeadlineIsNullOrderByCreatedAtDesc(today, pageable) :
+                postRepository.findAllByCategoryAndDeadlineGreaterThanEqualOrDeadlineIsNullOrderByCreatedAtDesc(category, today, pageable);
+        return posts.map(PostResponseDto::new);
+    }
+     // 모집글 상세 정보 조회
+    @Transactional(readOnly = true)
+    public PostResponseDto getPostDetail(Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. id=" + postId));
+        return new PostResponseDto(post);
+    }
+    // 새 모집글 등록
+    @Transactional
+    public Long createPost(Post request) {
+        return postRepository.save(request).getPostId();
+    }
+
+    // 게시글 내 모든 항목 수정 가능
+    @Transactional
+    public void updatePost(Long postId, String title, String content, Category category, String role, Integer count, LocalDate deadline) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. id=" + postId));
+
+        post.setTitle(title);
+        post.setContent(content);
+        post.setCategory(category);
+        post.setRole(role);
+        post.setTotalCount(count);
+        post.setDeadline(deadline);
+    }
+    // 내 게시글 삭제
+    @Transactional
+    public void deletePost(Long postId) {
+        postRepository.deleteById(postId);
+    }
+    // 내 게시글 조회
+    @Transactional(readOnly = true)
+    public Page<PostResponseDto> getMyPosts(String nickname, Pageable pageable) {
+        return postRepository.findAllByAuthorNicknameOrderByCreatedAtDesc(nickname, pageable)
+                .map(PostResponseDto::new);
+    }
+}
