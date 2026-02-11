f53ed22687204f1353915b99af272ca346ccabd4
Bae Suhyun <68588469+bsh-ko@users.noreply.github.com>
2026-02-11 15:54:02 +0900

    [bug] 프론트 배포 후 로그인 시 404 error (#141)
    * refactor: 소셜로그인 첫방문자인지 재방문자인지 구분
    
    * refactor: 타인 프로필 조회 시 파일 documents 배열 추가
    
    * bug: 파일 업로드 경로 재설정
    
    * refactor: 프론트 배포 주소 추가
    
    * refactor: 프로필 부분 수정
    
    * bug: config 파일 프론트 주소 정확히 다 추가

--- src/main/java/com/moayo/moayobackend/global/config/SecurityConfig.java
+++ src/main/java/com/moayo/moayobackend/global/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
-                    corsConfiguration.setAllowedOrigins(java.util.List.of("http://localhost:5173", "http://15.164.218.67.nip.io"));
+                    corsConfiguration.setAllowedOrigins(java.util.List.of("http://localhost:5173", "http://3.34.183.233.nip.io" , "https://moayo-frontend-soeun0127s-projects.vercel.app"));
                     corsConfiguration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS"));
                     corsConfiguration.setAllowedHeaders(java.util.List.of("*"));
                     corsConfiguration.setAllowCredentials(true);
