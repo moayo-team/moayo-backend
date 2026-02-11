74ddc170a3d2ef79a6a9d3ffdf4640f984a9ece6
bsh-ko <ssuh3872@gmail.com>
2026-02-11 15:49:40 +0900

    bug: config 파일 프론트 주소 정확히 다 추가

--- src/main/java/com/moayo/moayobackend/global/config/WebMvcConfig.java
+++ src/main/java/com/moayo/moayobackend/global/config/WebMvcConfig.java
@@ -24,7 +24,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://moayo-frontend-soeun0127s-projects.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
