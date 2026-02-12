52984e302081c1fd0ad134d8c7312b657dcec2fe
이서연 <redchk35@naver.com>
2026-02-12 14:04:11 +0900

    refactor: openAI 연동 구조 정리 및 환경변수 기반 설정 통일

--- src/main/java/com/moayo/moayobackend/MoayoBackendApplication.java
+++ src/main/java/com/moayo/moayobackend/MoayoBackendApplication.java
@@ -2,10 +2,12 @@ package com.moayo.moayobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class MoayoBackendApplication {
 
     public static void main(String[] args) {
